6ad09b262abb12eda387bbb24fb6a1bd7ad001ae
Juanes <juaneste2822@gmail.com>
2025-09-28 12:36:04 -0500

    Cambios aplicados en backend

--- src/main/kotlin/Databases.kt
+++ src/main/kotlin/Databases.kt
@@ -5,10 +5,14 @@ import org.jetbrains.exposed.sql.Database
 
 object Databases {
     fun init() {
-        val url = (System.getenv("DB_URL") ?: "jdbc:postgresql://localhost:5432/postgres") +
-                if (!(System.getenv("DB_URL")?.contains("sslmode") ?: false)) "?sslmode=require" else ""
+        val url = System.getenv("DB_URL")
+            ?: "jdbc:postgresql://localhost:5432/postgres"
         val user = System.getenv("DB_USER") ?: "postgres"
         val password = System.getenv("DB_PASSWORD") ?: "postgres"
+
+        println("DEBUG -> DB_URL=$url")
+        println("DEBUG -> DB_USER=$user")
+        println("DEBUG -> DB_PASSWORD length=${password.length}")
 
         Database.connect(
             url = url,
