a00944e12ac7cbf5b1c0671b20f42b2e010b93ff
Juanes <juaneste2822@gmail.com>
2025-09-28 10:36:45 -0500

    Cambios aplicados en backend

--- src/test/kotlin/ApplicationTest.kt
+++ src/test/kotlin/ApplicationTest.kt
@@ -4,17 +4,19 @@ import io.ktor.client.request.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
 import kotlin.test.Test
+import io.ktor.client.statement.*
 import kotlin.test.assertEquals
 
 class ApplicationTest {
 
     @Test
-    fun testRoot() = testApplication {
+    fun testEstado() = testApplication {
         application {
             module()
         }
-        client.get("/").apply {
+        client.get("/estado").apply {
             assertEquals(HttpStatusCode.OK, status)
+            assertEquals("CadMap backend activo", bodyAsText())
         }
     }
 
